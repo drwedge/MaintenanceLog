5ea8ed354c7833b4e31d0d6146d8853c451f6011
drwedge <chrisjbarton@gmail.com>
2015-11-27 08:14:44 -0800

    uploaded files

---
+++ app/src/main/java/com/example/cjbarton/maintenancelog/Oil.java
@@ -0,0 +1,14 @@
+package com.example.cjbarton.maintenancelog;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Oil extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oil);
+    }
+
+}
